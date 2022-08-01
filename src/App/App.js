e62a27339276b644160113ed7f1560221b042c44
gabriel <gabrielcastro.ifpr@gmail.com>
2022-08-01 17:36:28 -0300

    card props

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar/Navbar";
 import Hero from "../components/Hero/Hero";
 import Card from "../components/Card/Card"
+import cardImage01 from '../images/card01.png';
 import './App.css'
 
 function App(){
@@ -8,7 +9,14 @@ function App(){
         <div className="app">
             <Navbar />
             {/* <Hero /> */}
-            <Card />
+            <Card 
+                img = {cardImage01}
+                rating = {5.0}
+                reviewCount = {6}
+                country = 'Brazil'
+                title = 'Life lessons with Katie Zaferes'
+                price = {136.00}
+            />
         </div>
     )
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,19 +1,18 @@
-import cardImage from '../../images/card01.png';
 import { FaStar } from 'react-icons/fa';
 import './Card.css'
 
-function Card() {
+function Card(props) {
     return(
         <div className='card'>
-            <img src={cardImage} className='card-image' ></img>
+            <img src={props.img} className='card-image' ></img>
             <div className='card-opinion'>
                 <FaStar className='star'/>
-                <span>5.0</span>
-                <span className='gray'>(6) • </span>
-                <span className='gray'>USA</span>
+                <span>{ props.rating }</span>
+                <span className='gray'>({ props.reviewCount }) •</span>
+                <span className='gray'>{ props.country }</span>
             </div>
-            <p>Life lessons with Katie Zaferes</p>
-            <p><strong>From $136</strong> / person</p>
+            <p>{ props.title }</p>
+            <p><strong>From { props.price }</strong> / person</p>
         </div>
     );
 }
