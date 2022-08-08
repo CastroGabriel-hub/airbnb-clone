2b341fe9ea8ac008203918e08242b7b2363ffe39
gabriel <gabrielcastro.ifpr@gmail.com>
2022-08-08 16:02:31 -0300

    updt

--- src/App/App.js
+++ src/App/App.js
@@ -1,29 +1,30 @@
 import Navbar from "../components/Navbar/Navbar";
 import Hero from "../components/Hero/Hero";
 import Card from "../components/Card/Card"
-import cardImage01 from '../images/card01.png';
 import data from '../data';
 
 import './App.css'
 
 function App(){
-    const cards = data.map((item) => {
-        return <Card 
-                    img={item.image}
-                    rating={item.stats.rating}
-                    reviewCount={item.stats.reviewCount}
-                    location={item.location}
-                    title={item.title}
-                    price={item.price}
-                />
-    })
+    
 
     return (
         <div className="app">
             <Navbar />
             <Hero />
             <section className="cards-list">
-                {cards}
+
+                {data.map((item) => {
+                    return <Card 
+                        img={item.image}
+                        rating={item.stats.rating}
+                        reviewCount={item.stats.reviewCount}
+                        location={item.location}
+                        title={item.title}
+                        price={item.price}
+                    />
+                })}
+                
             </section>
         </div>
     )
